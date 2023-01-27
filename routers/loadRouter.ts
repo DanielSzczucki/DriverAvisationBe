bceb8b0cc93fbd0d8ebcd622cb39d306b877aa55
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-01-27 19:39:03 +0100

    add single gift info id and cunt

--- routers/loadRouter.ts
+++ routers/loadRouter.ts
@@ -1,16 +1,28 @@
 import { Router } from "express";
 import { LoadRecord } from "../record/load.record";
 import { DriverRecord } from "../record/driver.record";
+import { GetSingleLoadRes } from "../types";
 
 export const loadRouter = Router();
 
-loadRouter.get("/", async (req, res) => {
-  const loadList = await LoadRecord.listAll();
-  const driverList = await DriverRecord.listAll();
+loadRouter
+  .get("/", async (req, res) => {
+    const loadList = await LoadRecord.listAll();
+    const driverList = await DriverRecord.listAll();
 
-  res.json({
-    loadRouter: "ok",
-    loadList,
-    driverList,
+    res.json({
+      loadRouter: "ok",
+      loadList,
+      driverList,
+    });
+  })
+
+  .get("/:loadId", async (req, res) => {
+    const load = await LoadRecord.getOne(req.params.loadId);
+    const loadCount = await load.countGivenLoads();
+
+    res.json({
+      load,
+      loadCount,
+    } as GetSingleLoadRes);
   });
-});
